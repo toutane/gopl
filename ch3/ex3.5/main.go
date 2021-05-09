0dc9e6b0908617ec0f62c96aacf3b9b13fc68cde
Charles-Antoine LEGER <ca@leger.email>
2021-05-09 21:30:33 +0200

    Add exercise 3.5 (color mandelbrot set)

---
+++ ch3/ex3.5/main.go
@@ -0,0 +1,43 @@
+//Exercise 3.5 emits a colored PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		ymin, xmin, ymax, xmax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) representes complex value z.
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 10
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.RGBA{contrast * n, contrast * n, 0, 255}
+		}
+	}
+	//return color.Black
+	return color.RGBA{111, 0, 236, 255}
+}
