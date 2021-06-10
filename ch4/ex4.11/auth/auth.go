985fd61d984d20dc0a881e4269e3be3adc28bf1a
Charles-Antoine LEGER <ca@leger.email>
2021-06-10 23:08:40 +0200

    ex4.11 : Going up to step 2 in the auth Device Flow.

--- ch4/ex4.11/auth/auth.go
+++ ch4/ex4.11/auth/auth.go
@@ -1,10 +1,21 @@
 package auth
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os/exec"
 
 	"github.com/toutane/gopl/ch4/ex4.11/util"
 )
+
+const ClientID = "Iv1.1ae6ec7b4bd67430"
+
+type Device struct {
+	DeviceCode      string `json:"device_code"`
+	UserCode        string `json:"user_code"`
+	VerificationURI string `json:"verification_uri"`
+}
 
 func Status() string {
 	hosts, err := util.LoadHosts()
@@ -21,7 +32,40 @@ func Login() (string, error) {
 	if IsLogged() {
 		return fmt.Sprintf("You are already logged in !"), nil
 	}
+
+	codes, err := GetDevice()
+	if err != nil {
+		return fmt.Sprintf("Failed to ask the device code..."), err
+	}
+
+	fmt.Printf("Your code is: %s\n", codes.UserCode)
+	exec.Command("open", codes.VerificationURI).Start()
 	return "", nil
+}
+
+func GetDevice() (*Device, error) {
+	url := "https://github.com/login/device/code?client_id=" + ClientID
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	req.Header.Set("Accept", "application/vnd.github.v3.text-match+json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var result Device
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+
+	resp.Body.Close()
+	return &result, nil
 }
 
 func Logout(path string) (string, error) {
